1f726e81a9af8b3848f24d3c45212b98d4fe51a0
pierre.debin@student.vinci.be <&1XLViXm>
2020-11-01 17:20:19 +0100

    refactor - simpleinute

--- BerlinClock.php
+++ BerlinClock.php
@@ -3,13 +3,19 @@
 class BerlinClock
 {
 
+
     public function simpleMinute(String $minute) : String
     {
-        if($minute == "00") return  "OOOO";
-        else if($minute == "01" || $minute == "06") return  "YOOO";
-        else if($minute == "02" || $minute == "07") return  "YYOO";
-        else if($minute == "03" || $minute == "08") return  "YYYO";
-        else if($minute == "04" || $minute == "09") return  "YYYY";
+        $patern0 = '/^[0-5]0{1}$/';
+        $patern1_6 = '/^[0-5][1,6]{1}$/';
+        $patern2_7 = '/^[0-5][2,7]{1}$/';
+        $patern3_8 = '/^[0-5][3,8]{1}$/';
+        $patern4_9 = '/^[0-5][4,9]{1}$/';
+        if(preg_match($patern0 ,$minute)) return  "OOOO";
+        else if(preg_match($patern1_6 ,$minute)) return  "YOOO";
+        else if(preg_match($patern2_7 ,$minute)) return  "YYOO";
+        else if(preg_match($patern3_8 ,$minute)) return  "YYYO";
+        else if(preg_match($patern4_9 ,$minute)) return  "YYYY";
 
     }
 
