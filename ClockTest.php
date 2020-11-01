f86a26c86100bd03b6bc918aad5ab1a20fde58b5
pierre.debin@student.vinci.be <&1XLViXm>
2020-11-01 15:49:10 +0100

    add - simple minute for input 00

---
+++ ClockTest.php
@@ -0,0 +1,28 @@
+<?php
+require "vendor/autoload.php";
+
+use PHPUnit\Framework\TestCase;
+
+class ClockTest extends TestCase
+{
+    public function test_simpleMinute_given00m_shouldReturn0000(){
+        
+    }
+
+    public function test_simpleMinute_given01m_shouldReturnY000(){
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
