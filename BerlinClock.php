83ff7e3b88acb432e8465cb4f668eacea216fdd9
pierre.debin@student.vinci.be <&1XLViXm>
2020-11-01 16:16:23 +0100

    refactor - simple minute function

--- BerlinClock.php
+++ BerlinClock.php
@@ -6,10 +6,10 @@ class BerlinClock
     public function simpleMinute(String $minute) : String
     {
         if($minute == "00") return  "OOOO";
-        if($minute == "01") return  "YOOO";
-        if($minute == "02") return  "YYOO";
-        if($minute == "03") return  "YYYO";
-        if($minute == "04") return  "YYYY";
+        else if($minute == "01") return  "YOOO";
+        else if($minute == "02") return  "YYOO";
+        else if($minute == "03") return  "YYYO";
+        else if($minute == "04") return  "YYYY";
 
     }
 
