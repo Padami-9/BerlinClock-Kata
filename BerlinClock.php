26550dfc6609b23c89bec46ccbe12db49c4abbda
pierre.debin@student.vinci.be <&1XLViXm>
2020-11-02 16:07:56 +0100

    add - patern for preg_match for blockOf5minutes function

--- BerlinClock.php
+++ BerlinClock.php
@@ -3,32 +3,60 @@
 class BerlinClock
 {
 
+    public function berlinClock() : String
+    {
 
-
-    //$patern0 = '/^[0-5]0{1}$/';
-    //$patern1_6 = '/^[0-5][1,6]{1}$/';
-    //$patern2_7 = '/^[0-5][2,7]{1}$/';
-    //$patern3_8 = '/^[0-5][3,8]{1}$/';
-    //$patern4_9 = '/^[0-5][4,9]{1}$/';
-
-
-
-
+    }
 
     public function simpleMinute(String $minute) : String
     {
-        if($minute == "0") return  "OOOO";
-        else if($minute == "1" || $minute == "6") return  "YOOO";
-        else if($minute == "2" || $minute == "7") return  "YYOO";
-        else if($minute == "3" || $minute == "8") return  "YYYO";
-        else if($minute == "4" || $minute == "9") return  "YYYY";
+        $patern0_5 = '/^.[0,5]{1}$/';
+        $patern1_6 = '/^.[1,6]{1}$/';
+        $patern2_7 = '/^.[2,7]{1}$/';
+        $patern3_8 = '/^.[3,8]{1}$/';
+        $patern4_9 = '/^.[4,9]{1}$/';
+
+        if(preg_match($patern0_5, $minute)) return  "OOOO";
+        else if(preg_match($patern1_6, $minute)) return  "YOOO";
+        else if(preg_match($patern2_7, $minute)) return  "YYOO";
+        else if(preg_match($patern3_8, $minute)) return  "YYYO";
+        else if(preg_match($patern4_9, $minute)) return  "YYYY";
     }
 
     public function blockOf5Minutes(String $minute) : String
     {
-        if(preg_match('/^[0].$/', $minute)) {
-            if(preg_match('/^.[0]$/', $minute)) return "OOOOOOOOOOO";
-            else if(preg_match('/^.[5]$/', $minute)) return "YOOOOOOOOOO";
+        $patern0 = '/^[0].$/';
+        $patern1 = '/^[1].$/';
+        $patern2 = '/^[2].$/';
+        $patern3 = '/^[3].$/';
+        $patern4 = '/^[4].$/';
+        $patern5 = '/^[5].$/';
+        $patern0To4 = '/^.[0-4]{1}$/';
+        $patern5To9 = '/^.[5-9]{1}$/';
+
+        if(preg_match($patern0, $minute)) {
+            if(preg_match($patern0To4, $minute)) return "OOOOOOOOOOO";
+            else if(preg_match($patern5To9, $minute)) return "YOOOOOOOOOO";
+        }else
+            if(preg_match($patern1, $minute)) {
+            if(preg_match($patern0To4, $minute)) return "YYOOOOOOOOO";
+            else if(preg_match($patern5To9, $minute)) return "YYROOOOOOOO";
+        }else
+            if(preg_match($patern2, $minute)) {
+            if(preg_match($patern0To4, $minute)) return "YYRYOOOOOOO";
+            else if(preg_match($patern5To9, $minute)) return "YYRYYOOOOOO";
+        }else
+            if(preg_match($patern3, $minute)) {
+            if(preg_match($patern0To4, $minute)) return "YYRYYROOOOO";
+            else if(preg_match($patern5To9, $minute)) return "YYRYYRYOOOO";
+        }else
+            if(preg_match($patern4, $minute)) {
+            if(preg_match($patern0To4, $minute)) return "YYRYYRYYOOO";
+            else if(preg_match($patern5To9, $minute)) return "YYRYYRYYROO";
+        }else
+            if(preg_match($patern5, $minute)) {
+            if(preg_match($patern0To4, $minute)) return "YYRYYRYYRYO";
+            else if(preg_match($patern5To9, $minute)) return "YYRYYRYYRYY";
         }
     }
 
--- ClockTest.php
+++ ClockTest.php
@@ -15,79 +15,143 @@ class ClockTest extends TestCase
         $this->berlinClock = new BerlinClock();
     }
 
-
     public function test_simpleMinute_given00m_shouldReturnOOOO(){
-        $actual = $this->actSimpleMinute("0");
+        $actual = $this->actSimpleMinute("00");
 
         $this->assertEquals("OOOO", $actual);
     }
 
     public function test_simpleMinute_given01m_shouldReturnYOOO(){
-        $actual = $this->actSimpleMinute("1");
+        $actual = $this->actSimpleMinute("01");
 
         $this->assertEquals("YOOO", $actual);
     }
 
     public function test_simpleMinute_given02m_shouldReturnYYOO(){
-        $actual = $this->actSimpleMinute("2");
+        $actual = $this->actSimpleMinute("02");
 
         $this->assertEquals("YYOO", $actual);
     }
 
     public function test_simpleMinute_given03m_shouldReturnYYYO(){
-        $actual = $this->actSimpleMinute("3");
+        $actual = $this->actSimpleMinute("03");
 
         $this->assertEquals("YYYO", $actual);
     }
 
     public function test_simpleMinute_given04m_shouldReturnYYYY(){
-        $actual = $this->actSimpleMinute("4");
+        $actual = $this->actSimpleMinute("04");
 
         $this->assertEquals("YYYY", $actual);
     }
 
+    public function test_simpleMinute_given05m_shouldReturnOOOO(){
+        $actual = $this->actSimpleMinute("05");
+
+        $this->assertEquals("OOOO", $actual);
+    }
+
     public function test_simpleMinute_given06m_shouldReturnYOOO(){
-        $actual = $this->actSimpleMinute("6");
+        $actual = $this->actSimpleMinute("06");
 
         $this->assertEquals("YOOO", $actual);
     }
 
     public function test_simpleMinute_given07m_shouldReturnYYOO(){
-        $actual = $this->actSimpleMinute("7");
+        $actual = $this->actSimpleMinute("07");
 
         $this->assertEquals("YYOO", $actual);
     }
 
     public function test_simpleMinute_given08m_shouldReturnYYYO(){
-        $actual = $this->actSimpleMinute("8");
+        $actual = $this->actSimpleMinute("08");
 
         $this->assertEquals("YYYO", $actual);
     }
 
     public function test_simpleMinute_given09m_shouldReturnYYYY(){
-        $actual = $this->actSimpleMinute("9");
+        $actual = $this->actSimpleMinute("09");
 
         $this->assertEquals("YYYY", $actual);
     }
 
-    /*public function test_simpleMinute_given11m_shouldReturnYOOO(){
-        $actual = $this->actSimpleMinute("11");
+    public function test_simpleMinute_given10m_shouldReturnOOOO(){
+        $actual = $this->actSimpleMinute("10");
 
-        $this->assertEquals("YOOO", $actual);
-    }*/
+        $this->assertEquals("OOOO", $actual);
+    }
 
-    public function test_blockOf5Minutes_given00m_shouldReturnOOOOOOOOOOO(){
+    public function test_blockOf5Minutes_given00to04m_shouldReturnOOOOOOOOOOO(){
         $actual = $this->berlinClock->blockOf5Minutes("00");
 
         $this->assertEquals("OOOOOOOOOOO", $actual);
     }
 
-    public function test_blockOf5Minutes_given05m_shouldReturnYOOOOOOOOOO(){
+    public function test_blockOf5Minutes_given05to09m_shouldReturnYOOOOOOOOOO(){
         $actual = $this->berlinClock->blockOf5Minutes("05");
 
         $this->assertEquals("YOOOOOOOOOO", $actual);
     }
 
+    public function test_blockOf5Minutes_given10to14m_shouldReturnYYOOOOOOOOO(){
+        $actual = $this->berlinClock->blockOf5Minutes("11");
+
+        $this->assertEquals("YYOOOOOOOOO", $actual);
+    }
+
+    public function test_blockOf5Minutes_given15to19m_shouldReturnYYROOOOOOOO(){
+        $actual = $this->berlinClock->blockOf5Minutes("16");
+
+        $this->assertEquals("YYROOOOOOOO", $actual);
+    }
+
+    public function test_blockOf5Minutes_given20to24m_shouldReturnYYRYOOOOOOO(){
+        $actual = $this->berlinClock->blockOf5Minutes("22");
+
+        $this->assertEquals("YYRYOOOOOOO", $actual);
+    }
+
+    public function test_blockOf5Minutes_given25to29m_shouldReturnYYRYYOOOOOO(){
+        $actual = $this->berlinClock->blockOf5Minutes("27");
+
+        $this->assertEquals("YYRYYOOOOOO", $actual);
+    }
+
+    public function test_blockOf5Minutes_given30to34m_shouldReturnYYRYYROOOOO(){
+        $actual = $this->berlinClock->blockOf5Minutes("33");
+
+        $this->assertEquals("YYRYYROOOOO", $actual);
+    }
+
+    public function test_blockOf5Minutes_given35to39m_shouldReturnYYRYYRYOOOO(){
+        $actual = $this->berlinClock->blockOf5Minutes("38");
+
+        $this->assertEquals("YYRYYRYOOOO", $actual);
+    }
+
+    public function test_blockOf5Minutes_given40to44m_shouldReturnYYRYYRYYOOO(){
+        $actual = $this->berlinClock->blockOf5Minutes("44");
+
+        $this->assertEquals("YYRYYRYYOOO", $actual);
+    }
+
+    public function test_blockOf5Minutes_given45to49m_shouldReturnYYRYYRYYROO(){
+        $actual = $this->berlinClock->blockOf5Minutes("49");
+
+        $this->assertEquals("YYRYYRYYROO", $actual);
+    }
+
+    public function test_blockOf5Minutes_given50to54m_shouldReturnYYRYYRYYRYO(){
+        $actual = $this->berlinClock->blockOf5Minutes("50");
+
+        $this->assertEquals("YYRYYRYYRYO", $actual);
+    }
+
+    public function test_blockOf5Minutes_given55to59m_shouldReturnYYRYYRYYRYY(){
+        $actual = $this->berlinClock->blockOf5Minutes("55");
+
+        $this->assertEquals("YYRYYRYYRYY", $actual);
+    }
 
 
 
